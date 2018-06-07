a4927945e12dac2567797ce8e0a57549c3990f87
Kanyango <kariukikanyango@gmail.com>
2018-06-07 17:58:55 +0300

    Update car.ts

--- src/app/modules/modules/cars/car.ts
+++ src/app/modules/modules/cars/car.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Headers, Http } from '@angular/http';
+import {NgForm} from '@angular/forms';
+import { Car, Price } from '../utilities/car_model';
+
+import 'rxjs/add/operator/toPromise';
 
 @Component({
     selector: 'car-hire-component',
@@ -6,6 +12,15 @@ import { Component } from '@angular/core';
 })
 
 export class CarHireComponent {
+  
+  public cars: Array<any> = [];
+  model: any;
+  public carsUrl = 'https://kizusiadmin.herokuapp.com/car';
+  
+  constructor(private http: Http)
+    {
+     this.getCars();
+   }
     
     firstDayOfWeek: number = 0;
     mode: string = 'date';
@@ -15,6 +30,18 @@ export class CarHireComponent {
     selectedOption = '';
     arrival: string;
     
+  getCars(): Promise<any>
+  {
+    return this.http.get(this.carsUrl)
+                .toPromise()
+                .then(res => { this.cars = res.json(); console.log(this.cars)})
+                .catch(this.handleError);
+  }
+    private handleError(error: any): Promise<any>
+     {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+    }
     
 
 }
